a0b57b3be00cfb1721be273fb891b6be639a48c2
Toriolo <toriolo.net@gmail.com>
2026-06-25 09:17:14 +0800

    Implement Navbar background change on scroll
    Add script to change Navbar background on scroll

---
+++ images/script.js
@@ -0,0 +1,11 @@
+Mengubah latar belakang Navbar ketika di-scroll ke bawah
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+console.log("Script Toriolo 
